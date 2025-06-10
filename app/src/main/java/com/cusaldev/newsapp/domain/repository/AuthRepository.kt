6d01c5f472dc083d60e071a9857e23f885904ed4
Kusal Damsara <kusal2002d@Gmail.com>
2025-06-10 21:44:11 +0530

    Changes signOut to return a Result to handle potential errors.

--- app/src/main/java/com/cusaldev/newsapp/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/cusaldev/newsapp/domain/repository/AuthRepository.kt
@@ -5,6 +5,6 @@ import com.google.firebase.auth.FirebaseUser
 interface AuthRepository {
     suspend fun signIn(email: String, password: String): Result<FirebaseUser>
     suspend fun signUp(email: String, password: String): Result<FirebaseUser>
-    suspend fun signOut()
+    suspend fun signOut(): Result<Unit>
     fun getCurrentUser(): FirebaseUser?
 }
