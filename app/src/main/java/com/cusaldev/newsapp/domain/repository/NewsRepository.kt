7bd0ca1d88c744c35ee862da734f35528e8acf20
Kusal Damsara <kusal2002d@Gmail.com>
2025-06-10 23:30:38 +0530

    Refactor NewsRepository to return Flow of Result of Article list.

--- app/src/main/java/com/cusaldev/newsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/cusaldev/newsapp/domain/repository/NewsRepository.kt
@@ -1,9 +1,9 @@
 package com.cusaldev.newsapp.domain.repository
 
-import com.cusaldev.newsapp.domain.model.ArticleDto
-import com.cusaldev.newsapp.domain.model.Source
+import com.cusaldev.newsapp.domain.model.Article
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
-    suspend fun getTopHeadlines(): ArticleDto
+    suspend fun getTopHeadlines(): Flow<Result<List<Article>>>
+    suspend fun searchNews(query: String): Flow<Result<List<Article>>>
 }
