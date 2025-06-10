14b200c56c940352b290fb9c80b75bbe94c9ebe4
Kusal Damsara <kusal2002d@Gmail.com>
2025-06-10 23:30:54 +0530

    Refactor use case to return Flow of Result of Article List.

--- app/src/main/java/com/cusaldev/newsapp/domain/usecase/NewsTopHeadlineUseCase.kt
+++ app/src/main/java/com/cusaldev/newsapp/domain/usecase/NewsTopHeadlineUseCase.kt
@@ -1,11 +1,14 @@
 package com.cusaldev.newsapp.domain.usecase
 
+import com.cusaldev.newsapp.domain.model.Article
 import com.cusaldev.newsapp.domain.model.ArticleDto
 import com.cusaldev.newsapp.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class NewsTopHeadlineUseCase @Inject constructor(
     private val newsRepository: NewsRepository
 ) {
-    suspend operator fun invoke() : ArticleDto = newsRepository.getTopHeadlines()
+    suspend operator fun invoke() :
+            Flow<Result<List<Article>>> = newsRepository.getTopHeadlines()
 }
