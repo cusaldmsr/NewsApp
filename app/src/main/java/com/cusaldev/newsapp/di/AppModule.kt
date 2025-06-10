b1353e125c069dc76e285be96b4df3a5708b7b08
Kusal Damsara <kusal2002d@Gmail.com>
2025-06-10 19:37:13 +0530

    Define AuthRepository interface for authentication operations.

--- app/src/main/java/com/cusaldev/newsapp/di/AppModule.kt
+++ app/src/main/java/com/cusaldev/newsapp/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.cusaldev.newsapp.di
 
+import com.cusaldev.newsapp.data.remort.ApiInterceptors
 import com.cusaldev.newsapp.data.remort.NewsApiService
 import com.cusaldev.newsapp.data.repository.NewsRepositoryImpl
 import com.cusaldev.newsapp.domain.repository.NewsRepository
@@ -7,6 +8,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -17,9 +19,17 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): NewsApiService{
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(ApiInterceptors())
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): NewsApiService{
         return Retrofit.Builder()
             .baseUrl("https://newsapi.org/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(NewsApiService::class.java)
